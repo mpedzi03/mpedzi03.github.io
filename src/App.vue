f50ee42623e97c4b084bc25ac287c359cab9a4c4
Michael Pedzimaz <mpedzi03@gmail.com>
2021-02-22 14:34:02 -0600

    Refactoring pages

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,33 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
+  <div id="home">
       <the-header/>
-    </div>
-    <div class="row ">
-      <div class="col-1" id="social-nav-div">
+      <!-- <div id="social-nav-div">
         <the-social-nav/>
-      </div>
-      <div class="col-3" id="profile-div">
+      </div> -->
+      <div id="profile-div">
         <the-profile/>
       </div>
-      <div class="col-1" id="main-nav-div">
+      <!-- <div id="main-nav-div">
         <the-main-nav/>
-      </div>
-      <div class="col-7" id="main-content-div">
+      </div> -->
+      <!-- <div id="main-content-div">
         <router-view/>
-      </div>
-    </div>
+      </div> -->
   </div>
 </template>
 
 <script>
 import TheHeader from './components/shared/TheHeader.vue'
-import TheSocialNav from './components/shared/TheSocialNav.vue'
-import TheMainNav from './components/shared/TheMainNav.vue'
+// import TheSocialNav from './components/shared/TheSocialNav.vue'
+// import TheMainNav from './components/shared/TheMainNav.vue'
 import TheProfile from './components/shared/TheProfile.vue'
 
 export default {
   name: 'App',
   components: {
     TheHeader,
-    TheSocialNav,
-    TheMainNav,
+    // TheSocialNav,
+    // TheMainNav,
     TheProfile
   },
   data: function () {
@@ -57,80 +53,8 @@ export default {
   text-align: center;
 }
 
-body {
-  background-image: url('assets/bg_home.jpg');
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-  background-color: #464646;
-}
-
-#social-nav-div, #main-content-div, #main-nav-div, #profile-div {
-  margin-top: 3em;
-}
-#social-nav-div {
-  border-bottom: 2px solid black;
-  border-right: 2px solid black;
-}
-#main-content-div {
-  border-top: 2px solid black;
-}
-#main-nav-div {
-  border-left: 2px solid black;
-  border-bottom: 2px solid black;
-  border-right: 2px solid black;
-}
-#profile-div {
-  border-top: 2px solid black;
-}
-
-// .nav {
-//   position: sticky;
-//   top: 0;
-// }
-
-#profile-div {
-  display: flex;
-  justify-content: center;
-  align-items:center;
-}
-
-.nav-item {
-  // background-color: green;
-  border: 8px outset #f3f1f1;
-  // border-radius: 10px;
-  background: white;
-  margin-bottom: 30px;
-  font-size: 15px;
-  text-align: center;
-
-  .active {
-    // border-radius: 10px;
-    // background:#29293d;
-    background: #29293d;
-    border: 6px inset #f3f1f1;
-  }
-
-  :hover {
-    // border-radius: 10px;
-    background: #f4f1f1;
-  }
-}
-
-.nav-link {
-  color: #29293d;
-  font-weight: bold;
-}
-
-.nav-link.disabled {
-  color: white !important;
-}
-
-@media screen and (max-width: 1000px) {
-  .nav-button-text {
-    display: none;
-  }
+#home {
+  display: grid
 }
 
 </style>
--- src/components/shared/TheProfile.vue
+++ src/components/shared/TheProfile.vue
@@ -1,19 +1,68 @@
 <template>
-  <img id="profile-image" src="../../assets/profile_picture_8.jpg" height="250">
+  <div id="profile-section">
+    <img id="profile-image" src="../../assets/profile_picture_8.jpg" height="250">
+    <div>
+      <b-button id="contact-button" v-b-toggle.collapse-1 variant="primary" :pressed="contactBtnPressed" @click="contactBtnPressed = !contactBtnPressed">Contact Me!</b-button>
+      <b-collapse id="collapse-1" class="mt-2">
+        <div id="profile-contact">
+          <b-list-group horizontal>
+            <b-list-group-item tag="a" target="_blank">
+              Facebook <b-icon icon="facebook"/>
+            </b-list-group-item>
+            <b-list-group-item tag="a" target="_blank">
+              Twitter <b-icon icon="twitter"/>
+            </b-list-group-item>
+          </b-list-group>
+          <b-list-group horizontal>
+            <b-list-group-item tag="a" target="_blank">
+              LinkedIn <b-icon icon="linkedin"/>
+            </b-list-group-item>
+            <b-list-group-item tag="a" target="_blank">
+              Github <b-icon icon="github"/>
+            </b-list-group-item>
+          </b-list-group>
+        </div>
+        <!-- <b-nav vertical>
+          <b-nav-item href=""><b-icon icon="paperclip"/><br>Resume</b-nav-item>
+        </b-nav> -->
+      </b-collapse>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'TheProfile'
+    name: 'TheProfile',
+    data: function () {
+      return {
+        contactBtnPressed: false
+      }
+    }
 }
 </script>
 
-<style>
-#profile-image {
-   border-radius: 5%;
-   border: 5px solid black;
-   alt: "Should be a picture of me.. Awkward.";
-   text-align: center;
-   
-}
+<style lang="sass">
+#profile-image
+   border-radius: 5%
+   border: 5px solid black
+   alt: "Should be a picture of me.. Awkward."
+   text-align: center
+
+#profile-contact
+  width: 25rem
+
+#contact-button
+  width: 250px
+  background-color: #89adc7
+  color: #ffffff
+
+#contact-button:hover
+  background-color: #7692a6
+
+#contact-button:active
+  background-color: #536069
+
+#contact-button .active
+  background-color: #dddddd
+
 </style>
