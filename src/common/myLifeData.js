06dda55436b6b340aa4ddd91526b281907bb8ce4
Michael Pedzimaz <mpedzi03@gmail.com>
2021-03-19 11:42:04 -0500

    mysite reset my-life changes

---
+++ src/common/myLifeData.js
@@ -0,0 +1,56 @@
+export const lifeData = [
+  {
+    id: 0,
+    name: 'about me',
+    blob: `I am an inquisitive person always on the journey of finding out how things work. When I am faced with a problem, I find a solution.
+    When I find the solution, I work to improve it so that it is scalable, readable, and abides by the latest technological standards.
+    To keep myself grounded and staying positive, I enjoy board and video games with friends, snowboarding, and keeping up on the latest world news.`,
+    quote: `Perfection is impossible, but you don't stop aiming for it.`,
+    quoteAuthor: 'Saquon Barkley'
+  },
+  {
+    id: 1,
+    name: 'about me',
+    blob: `I am an inquisitive person always on the journey of finding out how things work. When I am faced with a problem, I find a solution.
+    When I find the solution, I work to improve it so that it is scalable, readable, and abides by the latest technological standards.
+    To keep myself grounded and staying positive, I enjoy board and video games with friends, snowboarding, and keeping up on the latest world news.`,
+    quote: `Perfection is impossible, but you don't stop aiming for it.`,
+    quoteAuthor: 'Saquon Barkley'
+  },
+  {
+    id: 2,
+    name: 'about me',
+    blob: `I am an inquisitive person always on the journey of finding out how things work. When I am faced with a problem, I find a solution.
+    When I find the solution, I work to improve it so that it is scalable, readable, and abides by the latest technological standards.
+    To keep myself grounded and staying positive, I enjoy board and video games with friends, snowboarding, and keeping up on the latest world news.`,
+    quote: `Perfection is impossible, but you don't stop aiming for it.`,
+    quoteAuthor: 'Saquon Barkley'
+  },
+  {
+    id: 3,
+    name: 'about me',
+    blob: `I am an inquisitive person always on the journey of finding out how things work. When I am faced with a problem, I find a solution.
+    When I find the solution, I work to improve it so that it is scalable, readable, and abides by the latest technological standards.
+    To keep myself grounded and staying positive, I enjoy board and video games with friends, snowboarding, and keeping up on the latest world news.`,
+    quote: `Perfection is impossible, but you don't stop aiming for it.`,
+    quoteAuthor: 'Saquon Barkley'
+  },
+  {
+    id: 4,
+    name: 'about me',
+    blob: `I am an inquisitive person always on the journey of finding out how things work. When I am faced with a problem, I find a solution.
+    When I find the solution, I work to improve it so that it is scalable, readable, and abides by the latest technological standards.
+    To keep myself grounded and staying positive, I enjoy board and video games with friends, snowboarding, and keeping up on the latest world news.`,
+    quote: `Perfection is impossible, but you don't stop aiming for it.`,
+    quoteAuthor: 'Saquon Barkley'
+  },
+  {
+    id: 5,
+    name: 'about me',
+    blob: `I am an inquisitive person always on the journey of finding out how things work. When I am faced with a problem, I find a solution.
+    When I find the solution, I work to improve it so that it is scalable, readable, and abides by the latest technological standards.
+    To keep myself grounded and staying positive, I enjoy board and video games with friends, snowboarding, and keeping up on the latest world news.`,
+    quote: `Perfection is impossible, but you don't stop aiming for it.`,
+    quoteAuthor: 'Saquon Barkley'
+  }
+]
