15971ce3112c1a00de96650922a11c680ec04551
Michael Pedzimaz <mpedzi03@gmail.com>
2021-03-30 20:21:01 -0500

    mysite updated banner pics

--- src/components/shared/TheBanner.vue
+++ src/components/shared/TheBanner.vue
@@ -1,8 +1,8 @@
 <template>
   <header>
     <picture>
-      <source srcset="@/assets/bg/bg_hobbies.jpg" media="(max-width: 575px)">
-      <img src="@/assets/bg/HomeBanner_large.jpeg" alt="banner image!" width="100%">
+      <source srcset="@/assets/bg/HomeBanner_small.jpg" media="(max-width: 575px)">
+      <img src="@/assets/bg/HomeBanner_large2.jpeg" alt="banner image!" width="100%">
     </picture>
   </header>
 </template>
